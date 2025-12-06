32311bd7c95a3e09df42503a8403d4af5824b2b7
tomasmark79 <tomas@digitalspace.name>
2025-12-06 20:29:43 +0100

    extracted responsibility

---
+++ src/SlashCommand/SlashCommand.cpp
@@ -0,0 +1,48 @@
+#include "SlashCommand.hpp"
+
+// Global commands definition
+
+const std::vector<SlashCommand> commands_ = {
+    {"ping", "get pong", "simple"},
+    {"help", "get help", "simple"},
+    {"emoji", "get emoji", "simple"},
+    {"addurl",
+     "add another RSS/ATOM feed URL",
+     {{OptionType::String, "url", "URL of the RSS/ATOM feed", true, {}, {}, {}},
+      {OptionType::Bool, "embedded", "Whether the feed should be embedded", false, {}, {}, {}}},
+     "rss"},
+    {"removeurl",
+     "remove an RSS/ATOM feed URL",
+     {{OptionType::String, "url", "URL of the RSS/ATOM feed to remove", true, {}, {}, {}}},
+     "rss"},
+    {"refetch", "manually refetch RSS/ATOM feeds", "rss"},
+    {"listurls", "get list of RSS/ATOM feed URLs", "rss"},
+    {"printfeed", "get random RSS/ATOM feed items", "rss"},
+    {"printcountfeeds", "get count of RSS/ATOM feed items", "rss"},
+    {"setstatus",
+     "set bot status message",
+     {{OptionType::String, "message", "The status message", true, {}, {}, {}}},
+     "botself"}};
+
+dpp::slashcommand SlashCommand::toDppCommand(dpp::snowflake bot_id) const {
+  dpp::slashcommand cmd(name_, description_, bot_id);
+
+  for (const auto &opt : options_) {
+    dpp::command_option dpp_opt(toCommandOptionType(opt.type), opt.name, opt.description,
+                                opt.required);
+
+    for (const auto &[choice_name, choice_value] : opt.choices) {
+      dpp_opt.add_choice(dpp::command_option_choice(choice_name, choice_value));
+    }
+
+    if (opt.min_value) dpp_opt.set_min_value(*opt.min_value);
+    if (opt.max_value) dpp_opt.set_max_value(*opt.max_value);
+
+    cmd.add_option(dpp_opt);
+  }
+
+  cmd.set_default_permissions(default_permissions_);
+  cmd.set_dm_permission(dm_permission_);
+
+  return cmd;
+}
---
+++ src/SlashCommand/SlashCommand.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <dpp/dpp.h>
+
+#include <cstdint>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+enum class OptionType : std::uint8_t { String, Integer, Bool, User, Channel, Role };
+
+class SlashCommand {
+public:
+  struct CommandOption {
+    OptionType type;
+    std::string name;
+    std::string description;
+    bool required = false;
+    
+    std::vector<std::pair<std::string, std::string>> choices;
+    std::optional<int64_t> min_value;
+    std::optional<int64_t> max_value;
+  };
+
+  /**
+   * @brief Construct a new Slash Command object
+   *
+   * @param n Name of the command
+   * @param d Description of the command
+   * @param h Handler type
+   */
+  SlashCommand(std::string n, std::string d, std::string h = "")
+      : name_(std::move(n)), description_(std::move(d)), handler_type_(std::move(h)) {}
+
+  /**
+   * @brief Construct a new Slash Command object with options
+   *
+   * @param n Name of the command
+   * @param d Description of the command
+   * @param opts Options for the command
+   * @param h Handler type
+   */
+  SlashCommand(std::string n, std::string d, std::vector<CommandOption> opts, std::string h = "")
+      : name_(std::move(n)), description_(std::move(d)), options_(std::move(opts)),
+        handler_type_(std::move(h)) {}
+
+  [[nodiscard]] const std::string &getName() const { return name_; }
+  [[nodiscard]] const std::string &getDescription() const { return description_; }
+  [[nodiscard]] const std::string &getHandlerType() const { return handler_type_; }
+  [[nodiscard]] const std::vector<CommandOption> &getOptions() const { return options_; }
+
+  SlashCommand &setDefaultPermissions(dpp::permission perms) {
+    default_permissions_ = perms;
+    return *this;
+  }
+
+  SlashCommand &setDmPermission(bool allowed) {
+    dm_permission_ = allowed;
+    return *this;
+  }
+
+  /**
+   * @brief Convert to dpp::slashcommand
+   *
+   * @param bot_id
+   * @return dpp::slashcommand
+   */
+  [[nodiscard]] dpp::slashcommand toDppCommand(dpp::snowflake bot_id) const;
+
+private:
+  std::string name_;
+  std::string description_;
+  std::vector<CommandOption> options_;
+  std::string handler_type_;
+
+  dpp::permission default_permissions_ = 0;
+  bool dm_permission_ = true;
+
+  static dpp::command_option_type toCommandOptionType(OptionType type) {
+    switch (type) {
+    case OptionType::String: return dpp::co_string;
+    case OptionType::Integer: return dpp::co_integer;
+    case OptionType::Bool: return dpp::co_boolean;
+    case OptionType::User: return dpp::co_user;
+    case OptionType::Channel: return dpp::co_channel;
+    case OptionType::Role: return dpp::co_role;
+    default: throw std::invalid_argument("Unknown OptionType");
+    }
+  }
+};
+
+extern const std::vector<SlashCommand> commands_;
